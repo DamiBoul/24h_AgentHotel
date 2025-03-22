af5ae20f3374ca57f82249bb661adfb82dc5d0d5
DamiBoul <damien.boulanger2@gmail.com>
2025-03-22 14:56:49 +0100

    debut api client

---
+++ distant/ClientAPI.py
@@ -0,0 +1,8 @@
+import requests
+
+class ClientAPI:
+    def __init__(self):
+        self.url = "https://app-584240518682.europe-west9.run.app/api/clients/"
+
+    def getClient(self, id):
+        return requests.get(self.url+"1")
