ff539a9d733d0814362c63ad85ee8b043b542be7
DamiBoul <damien.boulanger2@gmail.com>
2025-03-23 05:23:37 +0100

    refacto clientAPI

--- distant/ClientAPI.py
+++ distant/ClientAPI.py
@@ -11,3 +11,24 @@ class ClientAPI:
 
     def getClients(self):
         return httpx.get(self.url, headers=self.headers).json()
+
+    async def postClient(self, client: Models.ClientModel):
+        client_data = {
+            "name": client.name,
+            "phone_number": client.phone_number,
+            "room_number": client.room_number,
+            "special_requests": client.special_requests
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.post(self.url, json=client_data, headers=self.headers)
+        if response.status_code != 201:
+            raise Exception(response.text)
+        return {"message": "Client crée avec succès", "client": client_data}
+
+    def reserverClient(self, name, phone_number, room_number, special_requests):
+        modelClient = Models.ClientModel()
+        modelClient.name = name
+        modelClient.phone_number = phone_number
+        modelClient.room_number = room_number
+        modelClient.special_requests = special_requests
+        return self.postClient(modelClient)
--- repertory/__main__.py
+++ repertory/__main__.py
@@ -48,35 +48,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-url = "https://app-584240518682.europe-west9.run.app/api/clients/"
-headers = {"Authorization": "Token bvmgSMAjLpqk6PAyWJ86s62sxdXlbWlC"}
-
-
-@app.get("/restaurants/")
-async def getRestaurants():
-    return restaurantAPI.getRestaurants()
-
-
-@app.get("/clients/")
-async def getClients():
-    return clientAPI.getClients()
-
-
-@app.post("/client/")
-async def postClient(client: Models.ClientModel):
-    client_data = {
-        "name": client.name,
-        "phone_number": client.phone_number,
-        "room_number": client.room_number,
-        "special_requests": client.special_requests
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, json=client_data, headers=headers)
-    if response.status_code != 201:
-        raise Exception(response.text)
-    return {"message": "Client crée avec succès", "client": client_data}
-
-
 @app.post("/message/")
 async def postMessage(message: Models.MessageModel):
     
