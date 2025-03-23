f79e9f24b3a16973a52e14bed35532daa625b3f4
loli-lambda <anat07du72@gmail.com>
2025-03-23 04:03:51 +0100

    2h pour cette merde

---
+++ lang/emotiontoolcall.py
@@ -0,0 +1,28 @@
+from typing_extensions import Annotated, TypedDict
+
+class SPA(TypedDict):
+    """Quand la discussion tourne autours des spas
+    answer is answer you would give
+    """
+    answer : Annotated[str, ..., "answer you would give"]
+    
+class CRY(TypedDict):
+    """to use absolutely if you don't have any data, it's the best choice if you don't have anything labeled data
+    """
+    answer : Annotated[str, ..., "answer you would give"]
+
+class TOURISME(TypedDict):
+    """Si on parle de visiter autours de l'hotel
+    answer is answer you would give
+    """
+    answer : Annotated[str, ..., "answer you would give"]
+
+class CUISINE(TypedDict):
+    """Si on parle de restaurant ou de gastronomie
+    answer is answer you would give
+    """
+    answer : Annotated[str, ..., "answer you would give"]
+
+
+
+STATE = [CUISINE, SPA, TOURISME, CRY]
